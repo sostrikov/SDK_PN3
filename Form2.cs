b179886767d29216ad238851e3469d4648568d52
ostrikov <ostrikov@bmstu.ru>
2019-10-22 15:40:55 +0300

    Deserialization from file visitorsList

--- Form2.cs
+++ Form2.cs
@@ -7,9 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-
 using System.IO;
+using System.Xml.Serialization;
+using System.Reflection;
 using System.Drawing.Imaging;
 using System.Diagnostics;
 
@@ -33,6 +33,8 @@ namespace ParsecIntegrationClient
 			string domain = "SYSTEM";
 			string userName = "Ostrikov";
 			string password = "QAZwsx111";
+
+			//Подключаемся к серверу Parsec
 			SessionResult res = integrService.OpenSession(domain, userName, password);
 			//Guid SessionID = new Guid(txtCommonSessionID.Text);
 
@@ -51,38 +53,54 @@ namespace ParsecIntegrationClient
 
 
 			//1. Создаем физлицо будущего посетителя
+			
 			Person myGuest = new Person
 			{
 				ID = Guid.NewGuid(),
 				LAST_NAME = "Трамп2",
 				FIRST_NAME = "Дональд",
 				MIDDLE_NAME = "Обамович",
-				TAB_NUM = "guest-0002",
+				TAB_NUM = "guest-666",
 				ORG_ID = res.Value.RootOrgUnitID
 
 				//ORG_ID = Guid.Parse(myRootOrgUnitID)
 				
 			};
 
-			//2. Создаем посетителя из физлица (Голубая иконка)
+			//Сериализация в XML
+			
+			string directory = AppDomain.CurrentDomain.BaseDirectory;
+			string fileXmlName = Path.Combine(directory, "MyVizitors.xml");
+			Stream FileStreamXml = File.Create(fileXmlName);
+			XmlSerializer serializer = new XmlSerializer(typeof(Person));
+			serializer.Serialize(FileStreamXml, myGuest);
+			FileStreamXml.Close();
+
+			//Десериализация в XML
+			XmlSerializer serializer2 = new XmlSerializer(typeof(Person));
+			Stream FileStreamXml2 = File.OpenRead("MyVizitors.xml");
+			Person MyFileVisitor = (Person)serializer2.Deserialize(FileStreamXml2);
+			FileStreamXml.Close();
+
+			//2. Создаем посетителя (машину) из физлица (Голубая иконка)
 			//integrService.CreatePerson(mySessionIDGuid, myGuest);
 			//integrService.CreateVehicle(mySessionIDGuid, myGuest);
 			var res2 = integrService.CreateVisitor(mySessionIDGuid, myGuest);
 		
-			textBox2.Text = "Ok!";
+			textBox2.Text = MyFileVisitor.LAST_NAME;
 
 
 			//3. Создаем заявку на проход
 			VisitorRequest myVisitor = new VisitorRequest
 			{
-				PERSON_ID = myGuest.ID,
-				ORGUNIT_ID = myGuest.ORG_ID,
+				PERSON_ID = MyFileVisitor.ID,
+				ORGUNIT_ID = MyFileVisitor.ORG_ID,
 				NUMBER = 777,
 				PERSON_INFO = "Очень важный пассажир",
 				PURPOSE = "Деловая", //Цель визита
 				//STATUS =,
-				ADMIT_START = new DateTime(2019, 10, 22),
-				ADMIT_END =new DateTime(2019, 10, 22),
+				ADMIT_START = new DateTime(2019, 10, 22, 9, 00, 00),
+				ADMIT_END =new DateTime(2019, 10, 22, 13, 00, 00),
 				DATE = new DateTime(2019, 10, 22)
 			};
 
